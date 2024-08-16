02f72556eb294a6db29c28e8a6e8970e88ad38db
Nastya <nastasyz@ukr.net>
2024-08-16 09:11:25 +0300

    initial commit

---
+++ app/products/page.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function ProductsPage() {
+	return (
+		<main className="min-h-screen">
+			<div className="max-w-7xl px-12 pt-16 pb-[100px] my-0 mx-auto">
+                <div>
+                    <h1>Products: 64</h1>
+                    <ul>
+                        <li>
+                            <select name="" id="">
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                            </select>
+                        </li>
+                        <li>
+                            <select name="" id="">
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                            </select>
+                        </li>
+                        <li>
+                            <select name="" id="">
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                            </select>
+                        </li>
+                    </ul>
+                    <input type="search" name="" id="" />
+                </div>
+				<div>ProductsPage</div>                
+                    <ul>
+                        <li>
+                            <div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </li>
+                    </ul>                
+			</div>
+		</main>
+	);
+}
